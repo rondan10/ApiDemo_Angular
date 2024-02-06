2a55f8a1df1fbdd288edce5a48c1a880c6314f66
Adrian Rondan <amrondanc@bcds.net>
2024-02-06 16:28:36 -0500

    Consume api using angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MatCardModule} from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../service/api.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+
+export class HomeComponent implements OnInit {
+
+  data : any []= [];
+
+  constructor(private apiService : ApiService) {}
+
+  ngOnInit(): void {
+    this.llenarData();
+
+  }
+
+  llenarData(){
+    this.apiService.getData().subscribe( data =>{
+      this.data = data;
+      console.log(this.data);
+    })
+  }
+
+}
